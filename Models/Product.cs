6ccf9a8e1a859bfa00370ad4ae9d2d0dd89211cf
salman1256@yahoo.com <salman1256@yahoo.com>
2022-05-13 10:51:56 +0530

    After adding Product Model & Empty Controller

---
+++ Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OurMVCpp.Models
+{
+    public class Product
+    {
+        public int PId { get; set; }
+        public string PName { get; set; }
+        public double PPrice { get; set; }
+        public DateTime PMfDate { get; set; }
+    }
+}
