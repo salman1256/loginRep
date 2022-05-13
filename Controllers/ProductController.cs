3fc508ece614bfddb6f3110d1e46af2cea35d08c
salman1256@yahoo.com <salman1256@yahoo.com>
2022-05-13 10:56:47 +0530

    Change in Controller

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OurMVCpp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,20 @@ namespace OurMVCpp.Controllers
         // GET: Product
         public ActionResult Index()
         {
-            return View();
+            List<Product> proList = new List<Product>()
+            { 
+            new Product
+            { PId=1,PName="Herbal Shampoo",PPrice=123.56,PMfDate=new DateTime(day:12,month:12,year:2012)},
+             new Product
+            { PId=1,PName="Dabur Honey",PPrice=123.56,PMfDate=new DateTime(day:12,month:12,year:2021)},
+              new Product
+            { PId=2,PName="Jambo Cooler",PPrice=3000.40,PMfDate=new DateTime(day:03,month:02,year:2022)},
+               new Product
+            { PId=4,PName="Laptop",PPrice=109000.50,PMfDate=new DateTime(day:12,month:04,year:2021)},
+                new Product
+            { PId=3,PName="M-Phone",PPrice=87000.50,PMfDate=new DateTime(day:20,month:09,year:2020)},
+            };
+            return View(proList);
         }
     }
 }
