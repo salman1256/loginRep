d2f958982939116edaef76585813dd245b16c81f
salman1256@yahoo.com <salman1256@yahoo.com>
2022-05-13 11:31:10 +0530

    Adding Login

--- Models/Login.cs
+++ Models/Login.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace OurMVCpp.Models
 {
     public class Login
-    {
+    {    [Required(ErrorMessage ="Name is Required")]
+         [Display(Name ="User Name")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Password is Required")]
+        [Display(Name = "User Password")]
+        [DataType(DataType.Password)]
+        public string  UserPwd { get; set; }
     }
 }
